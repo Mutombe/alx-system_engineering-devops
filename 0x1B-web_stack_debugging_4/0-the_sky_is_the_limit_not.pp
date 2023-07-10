f4d438fc6359efbba48b87d1fa801340410f6141
Mutombe <simbarashemutombe1@gmail.com>
2023-07-10 18:02:55 +0000

    limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,12 @@
-#Fix problem of high amount of requests
+# Fix problem of high amount of requests
 
 exec {'replace':
   provider => shell,
-  command => 'sudo sed -i "s/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before => Exec['restart'],
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
-
 
 exec {'restart':
   provider => shell,
-  command => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart',
 }
